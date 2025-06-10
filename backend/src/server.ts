a4035b2af53bddd3201d222019997ed1a57dd607
Tamojit Das <tamojitdas181007@gmail.com>
2025-06-10 14:02:00 +0530

    Application init

---
+++ backend/src/server.ts
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const corsOpts = {
+    origin: '*',
+    methods: [
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+        'PATCH',
+        'OPTIONS'
+    ],
+    allowHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'X-CSRF-Token',
+        'X-HTTP-Method-Override'
+    ],
+    credentials: true
+};
+
+app.use(cors(corsOpts));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/api/v1', (req: Request, res: Response) => {
+    res.send('Server Up & Running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-4xl text-blue-600">Frontend Running</h1>
+    </>
+  )
+}
+
+export default App
